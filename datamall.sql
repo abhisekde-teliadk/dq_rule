769c4d410f13175fe0c6909af1acfcc6faf0d838
abhisekde-teliadk <abhisek.de@teliacompany.com>
2020-05-21 16:23:39 +0200

    Classification

--- datamall.sql
+++ datamall.sql
@@ -1,44 +1,33 @@
-create table statements(statement_id int primary key not null, description text, statement text)
+create table statements(statement_id integer primary key autoincrement, description text, statement text);
 
-insert into statements values(1, "", "select subscriber_id from analytics.abt_subscriber_current group by subscriber_id having count(*) > 1"); 
-insert into statements values(2, "", "select count(*) from analytics.abt_subscriber_current where is_active is not true"); 
-insert into statements values(3, "", "select count(*) from analytics.abt_subscriber_current where active_record_flag is not true"); 
-insert into statements values(4, "", "select count(*) from analytics.abt_subscriber_current where status not in ('A','S')"); 
-insert into statements values(5, "", "select count_from_base - count_from_sub_current as active_diff from (select count(*) as count_from_sub_current from analytics.abt_subscriber_current ) as a, (select count(*) as count_from_base from  base.import_fokus_base_subscriber where sub_status in ('A','S')) as b"); 
-insert into statements values(6, "", "select count(*) from analytics.abt_subscriber_current"); 
-insert into statements values(7, "", "select subscriber_id from analytics.abt_subscriber_history where is_active is true group by 1 having count(*) > 1"); 
-insert into statements values(8, "", "select subscriber_id from analytics.abt_subscriber_history where active_record_flag is true group by 1 having count(*) > 1"); 
-insert into statements values(9, "", "select subscriber_id from analytics.abt_subscriber_history where is_last is true group by 1 having count(*) > 1"); 
-insert into statements values(10, "", "select subscriber_id from analytics.abt_subscriber_history where is_last_valid is true group by 1 having count(*) > 1"); 
-insert into statements values(11, "", "select count(*) from analytics.abt_subscriber_current a left outer join analytics.abt_subscriber_history b on a.subscriber_current_key = b.subscriber_current_key where b.subscriber_current_key is null"); 
-insert into statements values(12, "", "select soc_group, count(*) from analytics.abt_service_current where soc_group not in ('No_group','Topup') and soc_group is not NULL group by 1 order by 1"); 
-insert into statements values(13, "", "select count(*) from analytics.abt_intake_kpi where subscriber_history_key is null and service_history_key is null "); 
-insert into statements values(14, "", "select count(*) from analytics.abt_intake_kpi a left outer join analytics.abt_subscriber_history b on a.subscriber_history_key = b.subscriber_history_key where a.subscriber_history_key is not null and b.subscriber_history_key is null"); 
-insert into statements values(15, "", "select count(*) from analytics.abt_intake_kpi a left outer join analytics.abt_service_history b on a.service_history_key = b.service_history_key and b.soc_group = 'Extra_sim' where a.service_history_key is not null and b.service_history_key is null"); 
-insert into statements values(16, "", "select soc, sum(closing_stock) from analytics.abt_kpis_report where year(now())*100+month(now()) = month group by 1 order by 1"); 
-insert into statements values(17, "", "select * from analytics.abt_stock_kpi"); 
-insert into statements values(18, "", "select case when is_extra_sim is true then 'Ekstra sim' else 'Priceplan' end as type, sum(subs_qty) from analytics.abt_stock_kpi where stock_date = subdate(to_date(now()),1) group by 1 order by 1"); 
-insert into statements values(19, "", "select count(*) from analytics.abt_subscriber_current"); 
-insert into statements values(20, "", "select count(*) from analytics.abt_service_current where soc_group = 'Extra_sim'"); 
-insert into statements values(21, "", "select * from analytics.abt_intake_kpi where subscriber_id=206558"); 
-insert into statements values(22, "", "select * from analytics.abt_subscriber_history where subscriber_id=206558 order by start_date"); 
-insert into statements values(23, "", "select subscriber_id,count(*) from analytics.abt_subscriber_history where is_active=1 group by 1 having count(*) > 1"); 
-insert into statements values(24, "", "select subscriber_id,is_active,* from analytics.abt_subscriber_history where subscriber_id = 14277975"); 
-insert into statements values(25, "", "select 0"); 
-insert into statements values(26, "", "select 1"); 
-insert into statements values(27, "", "select null");
+insert into statements(description, statement) values("", "select subscriber_id from analytics.abt_subscriber_current group by subscriber_id having count(*) > 1"); 
+insert into statements(description, statement) values("", "select count(*) from analytics.abt_subscriber_current where is_active is not true"); 
+insert into statements(description, statement) values("", "select count(*) from analytics.abt_subscriber_current where active_record_flag is not true"); 
+insert into statements(description, statement) values("", "select count(*) from analytics.abt_subscriber_current where status not in ('A','S')"); 
+insert into statements(description, statement) values("", "select count_from_base - count_from_sub_current as active_diff from (select count(*) as count_from_sub_current from analytics.abt_subscriber_current ) as a, (select count(*) as count_from_base from  base.import_fokus_base_subscriber where sub_status in ('A','S')) as b"); 
+insert into statements(description, statement) values("", "select count(*) from analytics.abt_subscriber_current"); 
+insert into statements(description, statement) values("", "select subscriber_id from analytics.abt_subscriber_history where is_active is true group by 1 having count(*) > 1"); 
+insert into statements(description, statement) values("", "select subscriber_id from analytics.abt_subscriber_history where active_record_flag is true group by 1 having count(*) > 1"); 
+insert into statements(description, statement) values("", "select subscriber_id from analytics.abt_subscriber_history where is_last is true group by 1 having count(*) > 1"); 
+insert into statements(description, statement) values( "", "select subscriber_id from analytics.abt_subscriber_history where is_last_valid is true group by 1 having count(*) > 1"); 
+insert into statements(description, statement) values( "", "select count(*) from analytics.abt_subscriber_current a left outer join analytics.abt_subscriber_history b on a.subscriber_current_key = b.subscriber_current_key where b.subscriber_current_key is null"); 
+insert into statements(description, statement) values("", "select soc_group, count(*) from analytics.abt_service_current where soc_group not in ('No_group','Topup') and soc_group is not NULL group by 1 order by 1"); 
+insert into statements(description, statement) values("", "select 0"); 
+insert into statements(description, statement) values("", "select 1"); 
+insert into statements(description, statement) values("", "select null");
 
-create table checks(check_id int primary key not null, description text, statement_1 int not null, relation text, statement_2 int not null, foreign key(statement_1) references statements,  foreign key(statement_2) references statements);
-insert into checks values(1, "", 1, "=", 25);
-insert into checks values(2, "", 2, "=", 25); 
-insert into checks values(3, "", 3, "=", 25); 
-insert into checks values(4, "", 4, "=", 25); 
-insert into checks values(5, "", 5, "=", 25);
+create table checks(check_id integer primary key  autoincrement, description text, statement_1 integer not null, relation text, statement_2 integer not null, foreign key(statement_1) references statements,  foreign key(statement_2) references statements);
+insert into checks(description, statement_1, relation, statement_2) values("", 1, "=", 13);
+insert into checks(description, statement_1, relation, statement_2) values("", 2, "=", 11); 
+insert into checks(description, statement_1, relation, statement_2) values("", 3, "=", 11); 
+insert into checks(description, statement_1, relation, statement_2) values("", 4, "=", 11); 
 
-create table rules(rule_id int not null, description text, check_id int not null);
+
+create table rules(rule_id integer not null, description text, check_id integer not null);
 insert into rules values(1, "", 1); 
 insert into rules values(1, "", 2); 
 insert into rules values(1, "", 3); 
 insert into rules values(1, "", 4); 
-insert into rules values(1, "", 5);
 
+create table check_result(check_result_id integer primary key  autoincrement, check_id integer not null, result text, exec_date text not null, foreign key(check_id) references checks);
+create table rule_result(rule_result_id integer primary key  autoincrement, rule_id integer not null, result text, exec_date text not null, foreign key(rule_id) references rules);
--- rule.py
+++ rule.py
@@ -1,30 +1,58 @@
-from database import Impala, SQLite3
+from database import __impala__, SQLite3
 import pandas as pd
+from datetime import datetime
 
 class DQRule:
-    def check_rule(self, rule_id):
-        check_list = self.rules.join(other=self.checks, on="check_id", how="inner", lsuffix="_r")
-        q1 = pd.merge(check_list, self.statements, left_on='statement_1', right_on='statement_id', how="inner")
-        q2 = pd.merge(q1, self.statements, left_on='statement_2', right_on='statement_id', how="inner")
+    def check(self, rule_id):
+        exec_ts = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        result_1 = []
+        result_2 = []
+        result_c = []
+        result_r = 'True'
 
-        q2["statement_x"].map(lambda x: self.repo.run_sql(x))
-        q2["statement_y"].map(lambda x: self.repo.run_sql(x))
+        for l in self.__sql1__:
+            sql = l[2]
+            result_1.append(self.__repo__.run_sql(sql))
+
+        for l in self.__sql2__:
+            sql = l[2]
+            result_2.append(self.__repo__.run_sql(sql))
+
+        for i in range(0, len(self.__check__)):
+            relation = self.__check__[i][3]
+            if relation == "=":
+                result_c[i] = (result_1[i] == result_2 [i]).__str__()
+            elif relation == "<=":
+                result_c[i] = (result_1[i] <= result_2 [i]).__str__()
+            elif relation == ">=":
+                result_c[i] = (result_1[i] >= result_2 [i]).__str__()
+            elif relation == "in":
+                result_c[i] = '' #TODO
+            else:
+                result_c[i] = '' #TODO
+
+            sql = 'insert into check_result(check_id, result, exec_date) values({}, "{}", "{}")'.format(self.__check__[0], result_c[i], exec_ts)
+            self.__repo__.run_sql(sql)
+            
+        # a rule is ok if all __check__s in it are ok
+        for r in result_c:
+            if r == 'False':
+                result_r = r
+                break
         
-        #q2["out_x"] = self.repo.run_sql(q2["statement_x"])[0][0]
-        #q2["out_y"] = self.repo.run_sql(q2["statement_y"])[0][0]
-        #q2["result"] = q2["out_1"] == q2["out_2"]
-        print(q2)
-        self.repo.close()
+        sql = 'insert into rule_result(rule_id, result, exec_date) values({}, "{}", "{}")'.format(rule_id, result_r, exec_ts)
+        self.__repo__.run_sql(sql)
+        self.__repo__.close()
 
-    def __init__(self):
-        self.repo = SQLite3()
-        self.impala = Impala()
-        statement_array = self.repo.run_sql("select * from statements")
-        checks_array = self.repo.run_sql("select * from checks")
-        rules_array = self.repo.run_sql("select * from rules")
+        # final o/p
+        self.check_results = result_c
+        self.rule_result = result_r
 
-        self.statements = pd.DataFrame(data=statement_array, columns=["statement_id", "description", "statement"])
-        self.checks = pd.DataFrame(data=checks_array, columns=["check_id", "description", "statement_1", "relation","statement_2"])
-        self.rules = pd.DataFrame(data=rules_array, columns=["rule_id", "description", "check_id"])
-        #check_rule(1)
-        #repo.close()
+    def __init__(self, rule_id):
+        self.__repo__ = SQLite3()
+        self.__impala__ = __impala__()
+        self.__sql1__ = self.__repo__.run_sql("select s.* from rules r join checks c on c.checkid = r.check_id join statements s on (s.statement_id = c.statement_1) where r.rule_id = " + rule_id)
+        self.__sql2__ = self.__repo__.run_sql("select s.* from rules r join checks c on c.checkid = r.check_id join statements s on (s.statement_id = c.statement_2) where r.rule_id = " + rule_id)
+        self.__check__ = self.__repo__.run_sql("select c.* from rules r join checks c on c.checkid = r.check_id where r.rule_id = 1")
+        self.check_results = []
+        self.rule_result = ''
